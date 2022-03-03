c9c8ca677a6e8584a7062a59206ff519ee62cedf
mamiko <tokunaga8950@gmail.com>
2022-03-03 14:22:49 +0900

    #8 環境構築（修正）

---
+++ src/learning/new-wish.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>New Wish</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>My Wish</h2>
+    <input type="textarea">
+    <h2>Memo</h2>
+    <input type="textarea">
+    <input type="submit" value="Wishを追加">
+
+</body>
+</html>
