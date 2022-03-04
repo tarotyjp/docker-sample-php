7401e8c8573a26352dead273cca1a6ff3feed0ea
mamiko <tokunaga8950@gmail.com>
2022-03-04 15:03:47 +0900

    #11 テーブル作成

---
+++ src/sql/wish-list.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: db
+-- 生成日時: 2022 年 3 月 04 日 05:53
+-- サーバのバージョン： 5.7.36
+-- PHP のバージョン: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `wish_list`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `wishs`
+--
+
+CREATE TABLE `wishs` (
+  `id` int(11) NOT NULL,
+  `my_wish` int(20) NOT NULL,
+  `memo` int(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
