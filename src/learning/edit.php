f78d35ca51087a5539bbf43adeae8adea757a5dd
mamiko <tokunaga8950@gmail.com>
2022-03-31 22:48:51 +0900

    ＃23 Wish編集（修正作業完了）

--- src/learning/edit.php
+++ src/learning/edit.php
@@ -15,7 +15,7 @@ try {
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     try {
-        //idを取得
+        //GETのidを取得
         $id = $_GET['id'];
 
 //    編集画面に内容を表示する
@@ -23,7 +23,6 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
         $stmt = $dbh->prepare($sql);
         $stmt->bindParam('id', $id);
         $stmt->fetchAll(PDO::FETCH_ASSOC);
-//        $wishes = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $stmt->execute();
     } catch (PDOException $e) {
         echo $e->getMessage();
@@ -31,24 +30,25 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     }
 }
 
-//if ($_SERVER['REQUEST_METHOD'] != 'GET') {
-    if ($myWish )) {
+if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+    if (!empty($_POST['myWish'])) {
         $id = $_POST['id'];
         $myWish = $_POST['myWish'];
         $memo = $_POST['memo'];
 
+//        編集内容を更新する処理
         $sql = 'UPDATE wishes SET my_wish = :MY_WISH, memo = :MEMO WHERE id = :id';
         $stmt = $dbh->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':MY_WISH', $myWish);
-        $stmt->bindParam(':MEMO', $memo);
+        $stmt->bindParam('id', $id);
+        $stmt->bindParam('MY_WISH', $myWish);
+        $stmt->bindParam('MEMO', $memo);
         $stmt->execute();
-//        header("Location: index.php");
+        header("Location: index.php");
     } else {
-        echo '更新できませんでした';
+        echo '更新できませんでした。My Wishを入力してください。';
         exit;
     }
-//}
+}
 
 ?>
 
@@ -63,7 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 </head>
 <body>
 <h1>Edit Wish</h1>
-<form method="POST" action="index.php">
+<form method="POST" action="edit.php">
     <?php
     if ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
         <span class="item">My Wish:</span><br>
@@ -77,9 +77,10 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
                 echo $row['memo']; ?></textarea></label><br>
         <!--      DB更新する為にidを渡す-->
         <input type="hidden" name="id" value="<?php
-        echo $id; ?>">
+        echo $row['id']; ?>">
         <input type="submit" class="btn-style" value="変更">
     <?php
     endif; ?>
-</form></body>
+</form>
+</body>
 </html>
